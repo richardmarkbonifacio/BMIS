fe268ca1aa0e9ffb6f4489b720c248170521268f
richardmarkbonifacio <richardbonifacio1015@gmail.com>
2022-06-13 06:29:06 +0800

    updated sql

--- Sql Database/db_barangay_old.sql
+++ Sql Database/db_barangay_old.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 13, 2022 at 12:27 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Oct 22, 2021 at 07:00 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -123,54 +123,7 @@ CREATE TABLE `tbllogs` (
 
 INSERT INTO `tbllogs` (`id`, `user`, `logdate`, `action`) VALUES
 (1, 'Administrator', '2021-10-21 16:27:24', 'Added Resident named Suares, Jude Reyes'),
-(2, 'Administrator', '2021-10-22 12:16:56', 'Update Resident named Suares, Jude Reyes'),
-(3, 'Administrator', '2022-06-09 22:06:31', 'Added Resident named dsasda, dasdas dasdas'),
-(4, 'Administrator', '2022-06-09 22:08:15', 'Update Resident named dsasda, dasdas dasdas'),
-(5, 'Administrator', '2022-06-09 22:09:12', 'Added Resident named dasdas, dasdas '),
-(6, 'Administrator', '2022-06-09 22:09:21', 'Update Resident named dsasda, dasdas dasdas'),
-(7, 'Administrator', '2022-06-09 22:19:12', 'Added Resident named dasdasd, dasdas dasdasda'),
-(8, 'Administrator', '2022-06-09 22:19:48', 'Added Resident named dasdas, dasdas dasdasdas'),
-(9, 'Administrator', '2022-06-09 22:21:19', 'Added Resident named dasdas, dasda dasdas'),
-(10, 'Administrator', '2022-06-10 22:16:20', 'Added Resident named Mark, Alpha Beta'),
-(11, 'Administrator', '2022-06-12 15:05:40', 'Added Resident named dasdas, dasdas dasdas'),
-(12, 'Administrator', '2022-06-12 15:06:02', 'Added Resident named dasdas, dasdasd dasdas');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblnonresident`
---
-
-CREATE TABLE `tblnonresident` (
-  `id` int(100) NOT NULL,
-  `fnam` varchar(250) NOT NULL,
-  `lnam` varchar(250) NOT NULL,
-  `mnam` varchar(250) NOT NULL,
-  `bdate` varchar(250) NOT NULL,
-  `address` varchar(250) NOT NULL,
-  `contactno` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tblnonresident`
---
-
-INSERT INTO `tblnonresident` (`id`, `fnam`, `lnam`, `mnam`, `bdate`, `address`, `contactno`) VALUES
-(9, 'dasdas', 'dasdas', 'dasdas', '', ' Zone , , ', ''),
-(9, 'dasdas', 'dasdas', 'dasdas', '', ' Zone , , ', ''),
-(10, 'batungbakal', 'annie', 'sa', '', ' Zone , , ', ''),
-(11, 'batungbadsadasdakal', 'annie', 'sa', '', ' Zone , , ', ''),
-(12, 'batungbadsadasdakal', 'annie', 'sadasdasdas', '', ' Zone , , ', ''),
-(13, 'czxczxczx', 'cxzczxczxczxcz', 'cxzcz', '', ' Zone , , ', ''),
-(14, 'dasdas', 'dasdasda', 'dasdasdadasdasdas', '', ' Zone , , ', ''),
-(15, 'dasdasdsadasdasdsa', 'dasdasda', 'dasdasdadasdasdas', '', ' Zone , , ', ''),
-(16, 'dasdas', 'dasda', 'dasda', '', ' Zone , , ', ''),
-(17, 'dasdas', 'dasda', 'dasdadasdasdasdasdas', '', ' Zone , , ', ''),
-(18, 'dasdas', 'dasdasda', 'zASAs', '', ' Zone , , ', ''),
-(19, 'dasdas', 'dasdas', 'dasdada', '', ' Zone , , ', ''),
-(21, 'dsadas', 'dasdas', 'dsadas', '2010-10-10', ' Zone 123, , ', ''),
-(22, 'Richard Mark', 'Bonifacio', 'dasda', '2001-01-01', ' Zone 01, dsadas, dsadas', ''),
-(23, 'a', 'This is', 'test', '', ' Zone , , ', '');
+(2, 'Administrator', '2021-10-22 12:16:56', 'Update Resident named Suares, Jude Reyes');
 
 -- --------------------------------------------------------
 
@@ -273,12 +226,7 @@ CREATE TABLE `tblresident` (
 --
 
 INSERT INTO `tblresident` (`id`, `lname`, `fname`, `mname`, `bdate`, `bplace`, `age`, `barangay`, `zone`, `totalhousehold`, `differentlyabledperson`, `relationtohead`, `maritalstatus`, `bloodtype`, `civilstatus`, `occupation`, `monthlyincome`, `householdnum`, `lengthofstay`, `religion`, `nationality`, `gender`, `skills`, `igpitID`, `philhealthNo`, `highestEducationalAttainment`, `houseOwnershipStatus`, `landOwnershipStatus`, `dwellingtype`, `waterUsage`, `lightningFacilities`, `sanitaryToilet`, `formerAddress`, `remarks`, `image`, `username`, `password`) VALUES
-(0, 'dasdas', 'dasdasd', 'dasdas', '', '', 0, '', '', 0, '', '', '', '', '', '', 0, 0, 0, '', '', 'Male', '', 0, 0, 'No schooling completed', 'Own Home', 'Owned', '1st Option', 'Faucet', 'Electric', 'Water-sealed', '', '', 'default.png', '12312312312', ''),
-(1, 'Suares', 'Jude', 'Reyes', '2021-10-12', 'Brgy. Mambato, Himamaylan City', 0, 'Brgy.Tan-awan', '1', 10, 'yes', 'Brother', 'single', '0+', 'Single', 'Programmer', 300000, 1, 5, 'Catholic', 'Filipino', 'Male', 'Programming', 1122, 2147483647, 'Doctorate degree', 'Live with Parents/Relatives', 'Care Taker', '2nd Option', 'Deep Well', '2147483647', 'Water-sealed', 'brgy. enlcaro', 'None', '1634804844819_[Complete] Laravel Ecommerce Project with Source Code.png', 'jude', 'jude123'),
-(2, 'dsasda', 'dasdas', 'dasdas', '2021-10-12', '', 0, '', '', 1, '', '', '', '', '', '', 1, 121, 1, '', '', 'Male', '', 1, 1, 'Doctorate degree', 'Live with Parents/Relatives', 'Care Taker', '2nd Option', 'Deep Well', '1', 'Water-sealed', '', '', 'default.png', '', ''),
-(3, 'dasdas', 'dasdas', 'dasdasdas', '', '', 0, 'dsadas', 'dasda', 0, '', '', 'dasdas', '', '', '', 0, 1, 0, '', '', '', '', 0, 0, 'No schooling completed', 'Own Home', 'Owned', '1st Option', 'Faucet', 'Electric', 'Water-sealed', '', '', 'default.png', 'dsadsadasdas', ''),
-(4, 'dasdas', 'dasda', 'dasdas', '', '', 0, '', '', 0, '', '', '', '', '', '', 0, 0, 0, '', '', '', '', 0, 0, 'No schooling completed', 'Own Home', 'Owned', '1st Option', 'Faucet', 'Electric', 'Water-sealed', '', '', 'default.png', 'dasdasdadadadadsa', ''),
-(5, 'Mark', 'Alpha', 'Beta', '', '', 0, 'dsadas', '01', 0, '', '', 'single', '', '', '', 0, 0, 0, '', '', '', '', 0, 0, 'No schooling completed', 'Own Home', 'Owned', '1st Option', 'Faucet', 'Electric', 'Water-sealed', '', '', 'default.png', 'alphabeta', '');
+(1, 'Suares', 'Jude', 'Reyes', '2021-10-12', 'Brgy. Mambato, Himamaylan City', 0, 'Brgy.Tan-awan', '1', 10, 'yes', 'Brother', 'single', '0+', 'Single', 'Programmer', 300000, 1, 5, 'Catholic', 'Filipino', 'Male', 'Programming', 1122, 2147483647, 'Doctorate degree', 'Live with Parents/Relatives', 'Care Taker', '2nd Option', 'Deep Well', '2147483647', 'Water-sealed', 'brgy. enlcaro', 'None', '1634804844819_[Complete] Laravel Ecommerce Project with Source Code.png', 'jude', 'jude123');
 
 -- --------------------------------------------------------
 
@@ -455,7 +403,7 @@ ALTER TABLE `tblhousehold`
 -- AUTO_INCREMENT for table `tbllogs`
 --
 ALTER TABLE `tbllogs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tblofficial`
@@ -468,6 +416,12 @@ ALTER TABLE `tblofficial`
 --
 ALTER TABLE `tblpermit`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tblresident`
+--
+ALTER TABLE `tblresident`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tblstaff`
